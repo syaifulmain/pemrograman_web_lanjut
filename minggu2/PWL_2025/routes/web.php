a5fb153cf650038b39b10ace2f5a9a1489f14de4
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2025-02-23 12:11:52 +0700

    Route Group dan Route Prefixes

--- minggu2/PWL_2025/routes/web.php
+++ minggu2/PWL_2025/routes/web.php
@@ -63,3 +63,27 @@ $url = route('profile');
 
 // Generating Redirects...
 return redirect()->route('profile');
+
+// Route Group dan Route Prefixes
+
+Route::middleware(['first', 'second'])->group(function () {
+    Route::get('/', function () {
+        // Uses first & second middleware...
+    });
+
+    Route::get('/user/profile', function () {
+        // Uses first & second middleware...
+    });
+});
+
+Route::domain('{account}.example.com')->group(function () {
+    Route::get('user/{id}', function ($account, $id) {
+        //
+    });
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/user', [UserController::class, 'index']);
+    Route::get('/post', [PostController::class, 'index']);
+    Route::get('/event', [EventController::class, 'index']);
+});
