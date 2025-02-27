454dc275666ea555fd621e70170089d8cf8708d6
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2025-02-27 18:33:40 +0700

    Eloquent ORM

--- minggu3/PWL_POS/routes/web.php
+++ minggu3/PWL_POS/routes/web.php
@@ -19,3 +19,4 @@ Route::get('/', function () {
 
 Route::get('/level', [\App\Http\Controllers\LevelController::class, 'index']);
 Route::get('/kategori', [\App\Http\Controllers\KategoriController::class, 'index']);
+Route::get('/user', [\App\Http\Controllers\UserController::class, 'index']);
