07396e4727eff3355d5fa7297c31e0d1a7248583
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2025-03-08 14:23:21 +0700

    praktikum 2.1

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -24,17 +24,26 @@ class UserController extends Controller
 //        UserModel::where('username', 'customer-1')->update($data);
 
 //         JS4: Praktikum 1
-        $data = [
-            'level_id' => 2,
-//             'username' => 'manager_dua',
-//             'nama' => 'Manager 2',
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
+//        $data = [
+//            'level_id' => 2,
+////             'username' => 'manager_dua',
+////             'nama' => 'Manager 2',
+//            'username' => 'manager_tiga',
+//            'nama' => 'Manager 3',
+//            'password' => Hash::make('12345')
+//        ];
+//        UserModel::create($data);
+//
+//        $user = UserModel::all();
+//        return view('user', ['data' => $user]);
 
-        $user = UserModel::all();
+//        JS: Praktikum 2.1
+//        $user = UserModel::find(1);
+//        $user = UserModel::where('level_id', 1)->first();
+//        $user = UserModel::firstWhere('level_id', 1);
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
