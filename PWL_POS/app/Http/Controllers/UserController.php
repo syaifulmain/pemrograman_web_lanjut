10998b2145eb55b8afb10e810f470c75ada39e4a
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2025-03-08 17:02:00 +0700

    praktikum 2.6

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Js;
 
 class UserController extends Controller
 {
@@ -98,21 +99,67 @@ class UserController extends Controller
 //         $user->isClean(); // true
 //         dd($user->isDirty());
 
-        $user = UserModel::create([
-            'username' => 'manager11',
-            'nama' => 'Manager11',
-            'password' => Hash::make('12345'),
-            'level_id' => 2,
+//        $user = UserModel::create([
+//            'username' => 'manager11',
+//            'nama' => 'Manager11',
+//            'password' => Hash::make('12345'),
+//            'level_id' => 2,
+//        ]);
+//        $user->username = 'manager12';
+//
+//        $user->save();
+//
+//        $user->wasChanged(); // true
+//        $user->wasChanged('username'); // true
+//        $user->wasChanged(['username', 'level_id']); // true
+//        $user->wasChanged('nama'); // false
+//        dd($user->wasChanged(['nama', 'username'])); // true
+
+//        JS4: Praktikum 2.6
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
+    }
+
+    public function tambah()
+    {
+        return view('user_tambah');
+    }
+
+    public function tambah_simpan(Request $request)
+    {
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash::make('$request->password'),
+            'level_id' => $request->level_id
         ]);
-        $user->username = 'manager12';
+        return redirect('/user');
+    }
+
+    public function ubah($id)
+    {
+        $user = UserModel::find($id);
+        return view('user_udah', ['data' => $user]);
+    }
+
+    public function ubah_simpan($id, Request $request)
+    {
+        $user = UserModel::find($id);
+
+        $user->username = $request->username;
+        $user->nama = $request->nama;
+        $user->password = Hash::make('$request->password');
+        $user->level_id = $request->level_id;
 
         $user->save();
 
-        $user->wasChanged(); // true
-        $user->wasChanged('username'); // true
-        $user->wasChanged(['username', 'level_id']); // true
-        $user->wasChanged('nama'); // false
-        dd($user->wasChanged(['nama', 'username'])); // true
-        return view('user', ['data' => $user]);
+        return redirect('/user');
+    }
+
+    public function hapus($id)
+    {
+        $user = UserModel::find($id);
+        $user->delete();
+        return redirect('/user');
     }
 }
--- PWL_POS/routes/web.php
+++ PWL_POS/routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\LevelController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +20,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/level', [\App\Http\Controllers\LevelController::class, 'index']);
-Route::get('/kategori', [\App\Http\Controllers\KategoriController::class, 'index']);
-Route::get('/user', [\App\Http\Controllers\UserController::class, 'index']);
+Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
+
+//JS4
+Route::get('/user/tambah', [UserController::class, 'tambah']);
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
