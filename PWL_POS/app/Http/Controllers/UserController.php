b9b3963096c265f4f90829752408a7f123e3aae5
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2025-03-08 14:45:27 +0700

    praktikum 2.2

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -41,9 +41,13 @@ class UserController extends Controller
 //        $user = UserModel::find(1);
 //        $user = UserModel::where('level_id', 1)->first();
 //        $user = UserModel::firstWhere('level_id', 1);
-        $user = UserModel::findOr(20, ['username', 'nama'], function () {
-            abort(404);
-        });
+//        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+//            abort(404);
+//        });
+
+//        JS: Praktikum 2.2
+//        $user = UserModel::findOrFail(1);
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
