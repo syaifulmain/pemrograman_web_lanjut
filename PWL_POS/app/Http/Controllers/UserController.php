9a9a7934429015baca07e815877659e351a466d2
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2025-03-08 15:42:44 +0700

    praktikum 2.4

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -52,6 +52,25 @@ class UserController extends Controller
 //        JS: Praktikum 2.3
         $user = UserModel::where('level_id', 2)->count();
 //        dd($user);
+
+//         JS4: Praktimum 2.4
+//        $user = UserModel::firstOrCreate(
+//            [
+//                'username' => 'manager22',
+//                'nama' => 'Manager Dua Dua',
+//                'password' => Hash::make('12345'),
+//                'level_id' => 2
+//            ],
+//        );
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ],
+        );
+        $user->save();
         return view('user', ['data' => $user]);
     }
 }
