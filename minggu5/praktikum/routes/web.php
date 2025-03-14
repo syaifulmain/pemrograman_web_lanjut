44ca78e99d2d96e21bdb7a598ef8ceb2b5a9832f
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2025-03-15 01:56:34 +0700

    jobsheet 5 praktikum 3

--- minggu5/praktikum/routes/web.php
+++ minggu5/praktikum/routes/web.php
@@ -21,6 +21,8 @@ Route::get('/', function () {
 });
 
 //Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori/create', [KategoriController::class, 'create']);
+Route::post('/kategori', [KategoriController::class, 'store']);
 Route::get('/kategori', [KategoriController::class, 'index']);
 //Route::get('/user', [UserController::class, 'index']);
 //
