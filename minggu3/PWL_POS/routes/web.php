92d71fdfbc6ce5337c4d08ace45b3f3477434655
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2025-03-14 19:26:29 +0700

    move back

---
+++ minggu3/PWL_POS/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/level', [\App\Http\Controllers\LevelController::class, 'index']);
+Route::get('/kategori', [\App\Http\Controllers\KategoriController::class, 'index']);
+Route::get('/user', [\App\Http\Controllers\UserController::class, 'index']);
