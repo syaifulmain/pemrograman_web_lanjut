4fab881bfa0d929af7498379fbc52cb36fe600f6
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2025-03-22 13:06:44 +0700

    init

---
+++ minggu6/Template Form (AdminLTE), Server Validation, Client Validation, CRUD/app/Http/Controllers/KategoriController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\DataTables\KategoriDataTable;
+use App\Models\KategoriModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class KategoriController extends Controller
+{
+    public function index(KategoriDataTable $dataTable)
+    {
+        return $dataTable->render('kategori.index');
+    }
+
+    public function create()
+    {
+        return view('kategori.create');
+    }
+
+    public function store(Request $request)
+    {
+        KategoriModel::create([
+            'kategori_kode' => $request->kodeKategori,
+            'kategori_nama' => $request->namaKategori
+        ]);
+        return redirect('/kategori');
+    }
+
+    public function edit($id)
+    {
+        $kategori = KategoriModel::findOrFail($id);
+        return view('kategori.edit', compact('kategori'));
+    }
+
+    public function update(Request $request)
+    {
+        $kategori = KategoriModel::findOrFail($request->kategori_id);
+        $kategori->kategori_kode = $request->kodeKategori;
+        $kategori->kategori_nama = $request->namaKategori;
+        $kategori->save();
+
+        return redirect('/kategori');
+    }
+
+    public function delete($id)
+    {
+        $kategori = KategoriModel::findOrFail($id);
+        $kategori->delete();
+
+        return redirect('/kategori');
+    }
+}
---
+++ minggu6/Template Form (AdminLTE), Server Validation, Client Validation, CRUD/database/seeders/StokSeeder.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class StokSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('t_stok')->insert([
+            [
+                'barang_id' => 1,
+                'user_id' => 1,
+                'stok_tanggal' => '2023-01-01',
+                'stok_jumlah' => 100,
+            ],
+            [
+                'barang_id' => 2,
+                'user_id' => 1,
+                'stok_tanggal' => '2023-01-01',
+                'stok_jumlah' => 50,
+            ],
+            [
+                'barang_id' => 3,
+                'user_id' => 1,
+                'stok_tanggal' => '2023-01-01',
+                'stok_jumlah' => 200,
+            ],
+            [
+                'barang_id' => 4,
+                'user_id' => 1,
+                'stok_tanggal' => '2023-01-01',
+                'stok_jumlah' => 300,
+            ],
+            [
+                'barang_id' => 5,
+                'user_id' => 1,
+                'stok_tanggal' => '2023-01-01',
+                'stok_jumlah' => 150,
+            ],
+            [
+                'barang_id' => 6,
+                'user_id' => 1,
+                'stok_tanggal' => '2023-01-01',
+                'stok_jumlah' => 250,
+            ],
+            [
+                'barang_id' => 7,
+                'user_id' => 1,
+                'stok_tanggal' => '2023-01-01',
+                'stok_jumlah' => 100,
+            ],
+            [
+                'barang_id' => 8,
+                'user_id' => 1,
+                'stok_tanggal' => '2023-01-01',
+                'stok_jumlah' => 50,
+            ],
+            [
+                'barang_id' => 9,
+                'user_id' => 1,
+                'stok_tanggal' => '2023-01-01',
+                'stok_jumlah' => 200,
+            ],
+            [
+                'barang_id' => 10,
+                'user_id' => 1,
+                'stok_tanggal' => '2023-01-01',
+                'stok_jumlah' => 300,
+            ],
+        ]);
+    }
+}
