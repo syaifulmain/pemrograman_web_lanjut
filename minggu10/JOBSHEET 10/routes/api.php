0599d7c65b4da871a70cfe7b990d9c66ba6a9243
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2025-04-21 23:54:54 +0700

    Praktikum 4 – Implementasi CRUD dalam RESTful API

--- minggu10/JOBSHEET 10/routes/api.php
+++ minggu10/JOBSHEET 10/routes/api.php
@@ -18,6 +18,13 @@ Route::post('/register', \App\Http\Controllers\Api\RegisterController::class)->n
 Route::post('/login', \App\Http\Controllers\Api\LoginController::class)->name('login');
 Route::post('/logout', \App\Http\Controllers\Api\LogoutController::class)->name('logout');
 
+Route::get('/levels', [\App\Http\Controllers\Api\LevelController::class, 'index']);
+Route::post('/levels', [\App\Http\Controllers\Api\LevelController::class, 'store']);
+Route::get('/levels/{level}', [\App\Http\Controllers\Api\LevelController::class, 'show']);
+Route::put('/levels/{level}', [\App\Http\Controllers\Api\LevelController::class, 'update']);
+Route::delete('/levels/{level}', [\App\Http\Controllers\Api\LevelController::class, 'destroy']);
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
