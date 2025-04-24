e89f1f9b45a9df3182112aff6d562cd13a8480c8
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2025-04-25 03:26:44 +0700

    tugas js 10

--- minggu10/JOBSHEET 10/routes/api.php
+++ minggu10/JOBSHEET 10/routes/api.php
@@ -24,6 +24,30 @@ Route::get('/levels/{level}', [\App\Http\Controllers\Api\LevelController::class,
 Route::put('/levels/{level}', [\App\Http\Controllers\Api\LevelController::class, 'update']);
 Route::delete('/levels/{level}', [\App\Http\Controllers\Api\LevelController::class, 'destroy']);
 
+Route::prefix('/users')->group(function () {
+    Route::get('/', [\App\Http\Controllers\Api\UserController::class, 'index']);
+    Route::post('/', [\App\Http\Controllers\Api\UserController::class, 'store']);
+    Route::get('/{user}', [\App\Http\Controllers\Api\UserController::class, 'show']);
+    Route::put('/{user}', [\App\Http\Controllers\Api\UserController::class, 'update']);
+    Route::delete('/{user}', [\App\Http\Controllers\Api\UserController::class, 'destroy']);
+});
+
+Route::prefix('/barangs')->group(function () {
+    Route::get('/', [\App\Http\Controllers\Api\BarangController::class, 'index']);
+    Route::post('/', [\App\Http\Controllers\Api\BarangController::class, 'store']);
+    Route::get('/{barang}', [\App\Http\Controllers\Api\BarangController::class, 'show']);
+    Route::put('/{barang}', [\App\Http\Controllers\Api\BarangController::class, 'update']);
+    Route::delete('/{barang}', [\App\Http\Controllers\Api\BarangController::class, 'destroy']);
+});
+
+Route::prefix('/kategoris')->group(function () {
+    Route::get('/', [\App\Http\Controllers\Api\KategoriController::class, 'index']);
+    Route::post('/', [\App\Http\Controllers\Api\KategoriController::class, 'store']);
+    Route::get('/{kategori}', [\App\Http\Controllers\Api\KategoriController::class, 'show']);
+    Route::put('/{kategori}', [\App\Http\Controllers\Api\KategoriController::class, 'update']);
+    Route::delete('/{kategori}', [\App\Http\Controllers\Api\KategoriController::class, 'destroy']);
+});
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
