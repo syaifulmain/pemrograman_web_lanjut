30c62587faba1f0f3737a9b11bdab4a212292f9b
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2025-05-04 13:29:52 +0700

    B. INFORMASI FILE UPLOAD

--- minggu12/JOBSHEET 12/app/Http/Controllers/FileUploadController.php
+++ minggu12/JOBSHEET 12/app/Http/Controllers/FileUploadController.php
@@ -10,8 +10,25 @@ class FileUploadController extends Controller
     {
         return view('file-upload');
     }
-    public function prosesFileUpload(Request $request)
-    {
-        return "Proses File Upload";
+    public function prosesFileUpload(Request $request) {
+        // dump($request->berkas);
+        // dump($request->file('file'));
+        // return "Pemrosesan file upload di sini";
+
+        if ($request->hasFile('berkas')) {
+            echo "path(): " . $request->berkas->path();
+            echo "<br>";
+            echo "extension(): " . $request->berkas->extension();
+            echo "<br>";
+            echo "getClientOriginalExtension(): " . $request->berkas->getClientOriginalExtension();
+            echo "<br>";
+            echo "getMimeType(): " . $request->berkas->getMimeType();
+            echo "<br>";
+            echo "getClientOriginalName(): " . $request->berkas->getClientOriginalName();
+            echo "<br>";
+            echo "getSize(): " . $request->berkas->getSize();
+        } else {
+            echo "Tidak ada berkas yang diupload";
+        }
     }
 }
