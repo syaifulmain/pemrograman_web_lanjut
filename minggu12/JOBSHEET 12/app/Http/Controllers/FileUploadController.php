e0d42f88f939e610facd57cd1975b21caae17d34
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2025-05-04 13:36:50 +0700

    C. VALIDASI FILE UPLOAD

--- minggu12/JOBSHEET 12/app/Http/Controllers/FileUploadController.php
+++ minggu12/JOBSHEET 12/app/Http/Controllers/FileUploadController.php
@@ -10,25 +10,35 @@ class FileUploadController extends Controller
     {
         return view('file-upload');
     }
-    public function prosesFileUpload(Request $request) {
+
+    public function prosesFileUpload(Request $request)
+    {
         // dump($request->berkas);
         // dump($request->file('file'));
+        //AA
         // return "Pemrosesan file upload di sini";
 
-        if ($request->hasFile('berkas')) {
-            echo "path(): " . $request->berkas->path();
-            echo "<br>";
-            echo "extension(): " . $request->berkas->extension();
-            echo "<br>";
-            echo "getClientOriginalExtension(): " . $request->berkas->getClientOriginalExtension();
-            echo "<br>";
-            echo "getMimeType(): " . $request->berkas->getMimeType();
-            echo "<br>";
-            echo "getClientOriginalName(): " . $request->berkas->getClientOriginalName();
-            echo "<br>";
-            echo "getSize(): " . $request->berkas->getSize();
-        } else {
-            echo "Tidak ada berkas yang diupload";
-        }
+        // B
+//        if ($request->hasFile('berkas')) {
+//            echo "path(): " . $request->berkas->path();
+//            echo "<br>";
+//            echo "extension(): " . $request->berkas->extension();
+//            echo "<br>";
+//            echo "getClientOriginalExtension(): " . $request->berkas->getClientOriginalExtension();
+//            echo "<br>";
+//            echo "getMimeType(): " . $request->berkas->getMimeType();
+//            echo "<br>";
+//            echo "getClientOriginalName(): " . $request->berkas->getClientOriginalName();
+//            echo "<br>";
+//            echo "getSize(): " . $request->berkas->getSize();
+//        } else {
+//            echo "Tidak ada berkas yang diupload";
+//        }
+
+        // C
+        $request->validate([
+            'berkas' => 'required|file|image|max:500',
+        ]);
+        echo $request->berkas->getClientOriginalName() . "lolos validasi";
     }
 }
